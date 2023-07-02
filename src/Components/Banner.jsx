4f4b7086bf72029cb0df9219746789f6beb92a1d
aminul-islam01 <abiddut953@gmail.com>
2023-07-03 01:54:37 +0600

    update resume download link

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -25,7 +25,7 @@ const Banner = () => {
                 />
                 <p>Through my expertise as a MERN stack developer, I have the ability to build web applications that deliver exceptional results. By harnessing the power of MongoDB, Express.js, React.js, and Node.js, I create dynamic, efficient, and scalable websites that cater to your specific needs.</p>
                 <button className="btn btn-outline btn-primary mt-5">
-                    <a href="/public/Aminul Islam Biddut-Resume.pdf" download>Download Resume <FaArrowAltCircleDown className='inline-block'></FaArrowAltCircleDown> </a>
+                    <a href="/Aminul-Resume.pdf" download>Download Resume <FaArrowAltCircleDown className='inline-block'></FaArrowAltCircleDown> </a>
                 </button>
             </div>
             <div className='bg-green-transparent'>
